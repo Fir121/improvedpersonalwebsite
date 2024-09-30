e13730f6eb9f84dcbee94c611c4dd662650ac761
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-10-01 03:21:18 +0400

    improve gradient

--- src/components/main-head.tsx
+++ src/components/main-head.tsx
@@ -5,8 +5,8 @@ export function MainHeader() {
         return (
                 <motion.div 
                     className="w-fit mx-auto md:p-10 p-0 rounded-lg backdrop-blur-md shadow-2xl flex flex-col gap-5 md:border-8 border-0 border-b-8 pb-4 pt-4 border-t-8"
-                    initial={{borderImage: "linear-gradient(45deg, #2b0a6e, #320c7d) 1"}}
-                    animate={{borderImage: "linear-gradient(-45deg, #6e0a2d, #705d94) 1"}}
+                    initial={{borderImage: "linear-gradient(45deg, #24305E, #f8a13f) 1"}}
+                    animate={{borderImage: "linear-gradient(-45deg, #405ac4, #cd3c18) 1"}}
                     transition={{ borderImage: {
                         repeat: Infinity,
                         repeatType: "reverse",
