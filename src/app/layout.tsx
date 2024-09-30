9fbd2de4e702d770b9644e6d7219568a5469143a
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-10-01 03:32:33 +0400

    metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mohamed Firas Adil",
   description: "Personal Portfolio Website by Mohamed Firas Adil, an astute Computer Science student with a zeal for learning.",
+  keywords: "Mohamed Firas Adil,Firas Adil,Mohamed Firas,Firas,Personal Portfolio,Portfolio,technology,software,cli,gui,websites,apps,python",
+  openGraph: {
+    title: "Mohamed Firas Adil",
+    description: "Personal Portfolio Website by Mohamed Firas Adil, an astute Computer Science student with a zeal for learning.",
+    images: "/icon.png",
+    url: "https://www.firasadil.com",
+  }  
 };
 
 export default function RootLayout({
