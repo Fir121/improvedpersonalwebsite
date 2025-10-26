2ad47de6b6c70b8e05c5eb3f79c10fcba97e9543
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2025-10-26 18:52:04 +0400

    add glassy style to nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,7 +17,7 @@ export function DynamicNav({home}:{home: boolean}){
                     layout
                     onClick={() => setOpen(!open)}
                     onKeyPress={(e) => {if(e.key === "Enter") setOpen(!open)}}
-                    className="item z-20 relative bg-dark-accent rounded-full w-16 h-16 p-10 mx-auto flex justify-center items-center align-middle cursor-pointer pointer-events-auto"
+                    className="item z-20 relative bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg rounded-full w-16 h-16 p-10 mx-auto flex justify-center items-center align-middle cursor-pointer pointer-events-auto"
                     onAnimationComplete={() => setOpen(true)}
                     initial= {{ y: 40, opacity: 0 }}
                     animate= {{
@@ -25,37 +25,39 @@ export function DynamicNav({home}:{home: boolean}){
                         opacity: 1
                     }}
                     transition= {{
-                        duration: 0.4,
-                        ease: "easeInOut",
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 30
                     }}
                     aria-label="Expand Menu Button"
                     tabIndex={0}
                 >
-                    <motion.div layout className="bg-accent w-10 h-10 p-5 z-20 rounded-full pointer-events-none"/>
+                    <motion.div layout className="bg-white/20 w-10 h-10 p-5 z-20 rounded-full pointer-events-none"/>
                 </motion.div>            
             </div>
             <div className="absolute left-0 z-10 w-screen bottom-[5%] pointer-events-none">
                 <motion.div
                     layout
-                    className="relative bg-dark-accent h-16 mx-auto w-11/12 md:w-8/12 rounded-2xl max-w-4xl pointer-events-auto"
+                    className={((home)?"bg-white/20":"bg-black/30") + " relative backdrop-blur-lg border border-white/50 shadow-lg h-16 mx-auto w-11/12 md:w-8/12 rounded-2xl max-w-4xl pointer-events-auto"}
                     initial={{ scaleX: 0, display: "none" }}
                     animate={{ scaleX: open ? 1 : 0, display: open ? "block" : "none" }}
                     transition={{ 
-                        duration: 0.4,
-                        ease:  "easeInOut",
-                        delay: 0.05,
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 40,
+                        delay: 0.1,
                     }}
                 >
                     <div className="menu-parent rounded-2xl">
                         {
-                            (home) ? <Link href="/projects" className="menu-item rounded-tl-2xl rounded-bl-2xl"><GrProjects /><p>Projects</p></Link>
+                            (home) ? <Link href="/projects" className="menu-item rounded-tl-2xl rounded-bl-2xl item-1"><GrProjects /><p>Projects</p></Link>
                             : 
-                            <Link href="/" className="menu-item rounded-tl-2xl rounded-bl-2xl"><FaHome /><p>Home</p></Link>
+                            <Link href="/" className="menu-item rounded-tl-2xl rounded-bl-2xl item-1"><FaHome /><p>Home</p></Link>
                         }
-                        <Link href="/pdf/Mohamed Firas Adil.pdf" className="menu-item" target="_blank"><FaRegFilePdf /><p>Résumé</p></Link>
+                        <Link href="/pdf/Mohamed Firas Adil.pdf" className="menu-item item-2" target="_blank"><FaRegFilePdf /><p>Résumé</p></Link>
                         <div className="menu-break"></div>
-                        <Link href="https://www.linkedin.com/in/mohamed-firas" className="menu-item" target="_blank"><AiOutlineLinkedin /><p>LinkedIn</p></Link>
-                        <Link href="https://github.com/Fir121" className="menu-item rounded-tr-2xl rounded-br-2xl" target="_blank"><FaGithub /><p>GitHub</p></Link>
+                        <Link href="https://www.linkedin.com/in/mohamed-firas" className="menu-item item-3" target="_blank"><AiOutlineLinkedin /><p>LinkedIn</p></Link>
+                        <Link href="https://github.com/Fir121" className="menu-item rounded-tr-2xl rounded-br-2xl item-4" target="_blank"><FaGithub /><p>GitHub</p></Link>
                     </div>
                 </motion.div>
             </div>
